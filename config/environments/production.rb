f6d04c638dea3000d35e55127f94f8f1aff2837b
Bhargav Singapuri <bhsingapuri@gmail.com>
2020-12-23 21:32:33 +0800

    Added Default URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -125,6 +125,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Devise Action Mailer
+  config.action_mailer.default_url_options = { :host => 'warzonetourney.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
@@ -133,6 +134,8 @@ Rails.application.configure do
   :user_name            => ENV['gmail_username'],
   :password             => ENV['gmail_password'],
   :authentication       => "plain",
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :domain => 'warzonetourney.herokuapp.com'
   }
+  config.action_mailer.perform_deliveries = true
 end
