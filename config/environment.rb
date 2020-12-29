0a6481d5a3f1dc6f2ef34e771a8ad33991a180a7
Bhargav Singapuri <bhsingapuri@gmail.com>
2020-12-29 11:19:37 +0800

    Changed to Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :user_name            => 'apikey',
+  :password             => ENV['sendgridkey'],
+  :domain               => 'majulahsingapuri.github.io',
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,11 +129,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :user_name            => 'apikey',
+    :password             => ENV['sendgridkey'],
+    :domain               => 'majulahsingapuri.github.io',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
 end
